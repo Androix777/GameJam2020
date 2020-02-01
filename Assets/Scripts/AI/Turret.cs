db731e2142fa713e8a597a7a3a6bdf6de9ac71ec
Legio <zum.q@mail.ru>
2020-02-01 14:45:03 +0500

    Добавлена турель

---
+++ Assets/Scripts/AI/Turret.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    [SerializeField]
+    float range;
+
+    GameObject target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateTarget()
+    {
+        if (target == null && Vector2.Distance((Vector2)target.transform.position,(Vector2)transform.position) > range)
+        {
+
+        }
+    }
+}
