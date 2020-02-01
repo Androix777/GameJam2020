8f93a186ca1e1dc2376744034d769d89c834e676
Androix777 <androix77@gmail.com>
2020-02-01 09:36:00 +0500

    Добавлен ShootOnDeath

---
+++ Assets/Scripts/DeathEffects/ShootOnDeath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootOnDeath : MonoBehaviour, IDeathEffect
+{
+    [SerializeField] private Shooter shooter;
+    [SerializeField] private DeathCause allowedDeathCause;
+    public DeathCause AllowedDeathCause { get => allowedDeathCause; set => allowedDeathCause = value; }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ActivateEffect()
+    {
+        shooter.Shoot(transform.TransformPoint(Vector2.up) - transform.position, true);
+    }
+}
