ad1f9108b0dcb046612d9348b9fce7c2e8b9357f
Androix777 <androix77@gmail.com>
2020-02-02 15:09:46 +0500

    Изменено изображение рестарта

--- Assets/ReloadScene.cs
+++ Assets/ReloadScene.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ReloadScene : MonoBehaviour
 {
@@ -18,6 +19,7 @@ public class ReloadScene : MonoBehaviour
 
     public void Reload ()
     {
-        Application.LoadLevel(Application.loadedLevel);
+        Debug.Log("Enter");
+        SceneManager.LoadScene(0);
     }
 }
