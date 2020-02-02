d60daa67a6e078d76377c59f941d9668317d69ff
Airplane <letadllo@gmail.com>
2020-02-02 11:22:18 +0500

    Добавлены щит и генератор в скрипт улучшений

--- Assets/Scripts/Components/Upgrader.cs
+++ Assets/Scripts/Components/Upgrader.cs
@@ -5,7 +5,9 @@ using System;
 public class Upgrader : MonoBehaviour
 {
     public EntityType entityType;
-    List<string> towerUpgrades = new List<string>() {"UpgradeDamage", "UpgradeFireRate", "UpgradeRegenerate", "UpgradeSelfDamage"};
+    List<string> towerUpgrades = new List<string>() {"UpgradeDamage", "UpgradeFireRate", "UpgradeRegenerate", "UpgradeSelfDamage", "UpgradeExplosiveProjectiles", "UpgradeNumOfGuns"};
+    List<string> shieldUpgrades = new List<string>() {"UpgradeReflect", "UpgradeRegenerate", "UpgradeAbsorb"};
+    List<string> generatorUpgrades = new List<string>() {"UpgradeEnergy", "UpgradeRegenerate", "UpgradeAbsorb"};
 
     Stack<Component> upgrades = new Stack<Component>();
     void Start()
@@ -23,6 +25,18 @@ public class Upgrader : MonoBehaviour
         if(entityType == EntityType.Tower)
         {
             String upgr = towerUpgrades[UnityEngine.Random.Range(0, towerUpgrades.Count)];
+            Debug.Log(Type.GetType(upgr));
+            upgrades.Push(gameObject.AddComponent(Type.GetType(upgr)));
+        }
+        else if(entityType == EntityType.Wall)
+        {
+            String upgr = shieldUpgrades[UnityEngine.Random.Range(0, shieldUpgrades.Count)];
+            Debug.Log(Type.GetType(upgr));
+            upgrades.Push(gameObject.AddComponent(Type.GetType(upgr)));
+        }
+        else if(entityType == EntityType.Generator)
+        {
+            String upgr = generatorUpgrades[UnityEngine.Random.Range(0, generatorUpgrades.Count)];
             Debug.Log(Type.GetType(upgr));
             upgrades.Push(gameObject.AddComponent(Type.GetType(upgr)));
         }
