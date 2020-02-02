f046966fa2b401555817c9a2d1b9234b0058a538
Androix777 <androix77@gmail.com>
2020-02-02 13:27:35 +0500

    Добавлен счетчик энергии

---
+++ Assets/EnergyUpdater.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyUpdater : MonoBehaviour
+{
+    Core core;
+
+    void Start()
+    {
+        core = GameObject.FindWithTag("Core").GetComponent<Core>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
