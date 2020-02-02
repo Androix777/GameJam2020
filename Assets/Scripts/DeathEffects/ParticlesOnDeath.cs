7cc9af5d08c5d07e0e66d059fc50568447ba0e9c
Androix777 <androix77@gmail.com>
2020-02-02 12:55:45 +0500

    Частицы смерти

---
+++ Assets/Scripts/DeathEffects/ParticlesOnDeath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesOnDeath : MonoBehaviour, IDeathEffect
+{
+    [SerializeField] private DeathCause allowedDeathCause;
+    public DeathCause AllowedDeathCause { get => allowedDeathCause; set => allowedDeathCause = value; }
+    public GameObject psystem;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ActivateEffect()
+    {
+        Instantiate(psystem, gameObject.transform.position, Quaternion.identity);
+    }
+}
