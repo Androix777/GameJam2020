213cf47d01cc9e09210913ffb4a66760434e9477
Airplane <letadllo@gmail.com>
2020-02-02 11:09:56 +0500

    Добавлен апгрейд генератора

---
+++ Assets/Scripts/Upgrades/Generator/UpgradeEnergy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeEnergy : MonoBehaviour
+{
+    public int energyAmount = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Generator>().energy += energyAmount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy()
+    {
+        gameObject.GetComponent<Generator>().energy -= energyAmount;
+    }
+}
